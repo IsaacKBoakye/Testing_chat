200ec05a898624cdec4448de2a1d671c871dd493
Isaac Boakye <ikboakye2001@gmail.com>
2024-02-16 10:55:26 -0400

    updated git rul

--- app.test.js
+++ app.test.js
@@ -5,8 +5,17 @@ describe('Chat App Tests', () => {
 
   // Setup a socket connection before each test
   beforeEach((done) => {
-    // Connect to the server
-    socket = ioClient.connect('http://localhost:3000');
+    // Connect to the server using a dynamic port
+    // socket = ioClient.connect('XXXXXXXXXXXXXXXXXXXXX');
+    // socket = ioClient.connect('http://localhost:3000');
+
+      // Construct the GitHub Actions URL
+      const githubServerUrl = process.env.GITHUB_SERVER_URL;
+      const githubRunId = process.env.GITHUB_RUN_ID;
+      const githubUrl = `${githubServerUrl}/${githubRunId}`;
+   
+      // Connect to the server
+      socket = ioClient.connect(githubUrl);
 
     // Handle connection event
     socket.on('connect', () => {
